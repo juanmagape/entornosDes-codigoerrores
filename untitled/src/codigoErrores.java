08bd23c8ed8e41d231d47b2092204abf91203fd1
juanmagape <juanmagape.trb.@gmail.com>
2025-10-29 09:36:30 +0100

    añadir programa

---
+++ untitled/src/codigoErrores.java
@@ -0,0 +1,35 @@
+public class codigoErrores {
+     static voy main() {
+        Scanner k = Scanner(System.out);
+        int a = false;
+        int b;
+        Int resultado;
+        while (a) {
+           switch (c) {
+               int c = knextInt();
+
+               case 1 {
+                   System.out.printfn("valor");
+                   b = k nextBoolean();
+               }
+               case 2:
+                   Systemoutprintln('ac');
+                   resultado = System.out.println(b * 9/5 + 32)
+               case 3 {
+                   System.out('af');
+                    resultado = System.out.println(b -32 * "5/9");
+break;
+                }
+               case "4:
+                   "out.println(ak)";
+                   resultado = System.out.println(b - 32 * "5/9 + 273.15")
+                   break
+
+           }
+
+            System.out.println(verificando'')
+            System.out(" = " + "valor")
+            println("????")
+        }
+    }
+}
